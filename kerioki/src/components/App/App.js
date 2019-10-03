02eb6796b638218755fa45db45a5561d19a71e6f
tyhag-study <hagen.tyler.stuff@gmail.com>
2019-10-02 19:00:44 -0700

    Run create-react-app, add barebones SearchBar component

---
+++ kerioki/src/components/App/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+import SearchBar from '../SearchBar/SearchBar';
+
+function App() {
+  return (
+    <div className="App">
+      Hello!
+      <SearchBar />
+    </div>
+  );
+}
+
+export default App;
